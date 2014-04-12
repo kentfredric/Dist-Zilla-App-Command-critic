234a42312cb3c67ffdb808bea199480e9b7547aa
Kent Fredric <kentfredric@gmail.com>
2014-04-13 11:09:10 +1200

    Fix criticisms

--- lib/Dist/Zilla/App/Command/critic.pm
+++ lib/Dist/Zilla/App/Command/critic.pm
@@ -34,9 +34,9 @@ The result will be similar to doing:
 
    dzil run --no-build perlcritic -p perlcritic.rc lib/
 
-Except that is useless to me because it doesn't output the filenames anywhere unless you have a verbosity level that incorporates
-a filename in I<EACH> violation, which for me, is undesriable clutter when you have 20 violations in a single file. ( And the most
-L<< verbose violation levels|perlcritic/verbose-N-FORMAT >>, that is, all except C<1,2,3,5,7> lack C<%f>.
+Except that is useless to me because it doesn't output the file names anywhere unless you have a verbosity level that incorporates
+a file name in I<EACH> violation, which for me, is undesirable clutter when you have 20 violations in a single file. ( And the most
+L<< verbose violation levels|perlcritic/verbose-N-FORMAT >>, that is, all except C<1,2,3,5,7> lack C<%f> )
 
 =cut
 
