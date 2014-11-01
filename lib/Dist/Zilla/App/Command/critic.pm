cf93b94b26ccea02456bc407941fcf652e025a42
Kent Fredric <kentfredric@gmail.com>
2014-11-02 02:08:09 +1300

    Build results of a568b3c (on master)

--- lib/Dist/Zilla/App/Command/critic.pm
+++ lib/Dist/Zilla/App/Command/critic.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::App::Command::critic;
 
-our $VERSION = '0.001004';
+our $VERSION = '0.001005';
 
 # ABSTRACT: build your dist and run Perl::Critic on the built files.
 
@@ -196,7 +196,7 @@ Dist::Zilla::App::Command::critic - build your dist and run Perl::Critic on the 
 
 =head1 VERSION
 
-version 0.001004
+version 0.001005
 
 =head1 DESCRIPTION
 
